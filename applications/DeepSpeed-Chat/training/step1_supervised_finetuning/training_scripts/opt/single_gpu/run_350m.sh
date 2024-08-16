533fa5584761129369cb56d0f7c3296b7f22afb9
yashas-vaidya <yashas.vaidya@gmail.com>
2024-08-15 22:56:16 -0400

    fix model name

--- applications/DeepSpeed-Chat/training/step1_supervised_finetuning/training_scripts/opt/single_gpu/run_350m.sh
+++ applications/DeepSpeed-Chat/training/step1_supervised_finetuning/training_scripts/opt/single_gpu/run_350m.sh
@@ -18,7 +18,7 @@ if [ "$ZERO_STAGE" == "" ]; then
 fi
 mkdir -p $OUTPUT
 
-deepspeed --num_gpus 1 main.py --model_name_or_path facebook/opt-1.3b \
+deepspeed --num_gpus 1 main.py --model_name_or_path facebook/opt-350m \
    --gradient_accumulation_steps 8 --lora_dim 128 --zero_stage $ZERO_STAGE \
    --enable_tensorboard \
    --tensorboard_path $OUTPUT \
