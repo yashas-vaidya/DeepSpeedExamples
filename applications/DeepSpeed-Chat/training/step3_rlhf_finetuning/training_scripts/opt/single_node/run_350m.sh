5c5616fe466e76132010565cff613661d655a810
yashas-vaidya <yashas.vaidya@gmail.com>
2024-08-20 13:55:57 -0400

    add scripts for 350m

---
+++ applications/DeepSpeed-Chat/training/step3_rlhf_finetuning/training_scripts/opt/single_node/run_350m.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+# Copyright (c) Microsoft Corporation.
+# SPDX-License-Identifier: Apache-2.0
+
+# YV_
+# Copied from: applications/DeepSpeed-Chat/training/step3_rlhf_finetuning/training_scripts/opt/single_node/run_1.3b_lora.sh
+# Modified using: applications/DeepSpeed-Chat/training/step3_rlhf_finetuning/training_scripts/opt/single_gpu/run_1.3b.sh
+
+ACTOR_MODEL_PATH=$1
+CRITIC_MODEL_PATH=$2
+ACTOR_ZERO_STAGE=$3
+CRITIC_ZERO_STAGE=$4
+OUTPUT=$5
+
+if [ "$OUTPUT" == "" ]; then
+    OUTPUT=./output
+fi
+if [ "$ZERO_STAGE" == "" ]; then
+    ZERO_STAGE=0
+fi
+
+Num_Padding_at_Beginning=1 # this is model related
+
+Actor_Lr=9.65e-6
+Critic_Lr=5e-6
+
+mkdir -p $OUTPUT
+
+deepspeed --master_port 12346 main.py \
+   --data_path Dahoas/rm-static \
+   --data_split 2,4,4 \
+   --actor_model_name_or_path $ACTOR_MODEL_PATH \
+   --critic_model_name_or_path $CRITIC_MODEL_PATH \
+   --num_padding_at_beginning 1 \
+   --per_device_generation_batch_size 16 \
+   --per_device_training_batch_size 16 \
+   --generation_batches 1 \
+   --ppo_epochs 1 \
+   --max_answer_seq_len 512 \
+   --max_prompt_seq_len 256 \
+   --actor_learning_rate ${Actor_Lr} \
+   --critic_learning_rate ${Critic_Lr} \
+   --num_train_epochs 1 \
+   --lr_scheduler_type cosine \
+   --gradient_accumulation_steps 1 \
+   --num_warmup_steps 100 \
+   --deepspeed --seed 1234 \
+   --actor_dropout 0.0 \
+   ${ACTOR_ZERO_STAGE} \
+   ${CRITIC_ZERO_STAGE} \
+   --actor_lora_dim 128 \
+   --enable_hybrid_engine \
+   --actor_lora_module_name decoder.layers. \
+   --output_dir $OUTPUT \
+    &> $OUTPUT/training.log
